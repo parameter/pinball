2ad0f7d39ce4f73e46e92925e650d398af4d4207
parameter <paer.henriksson@yahoo.com>
2019-03-25 12:28:34 +0100

    ...

---
+++ Assets/PaddleLeftScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleLeftScript : MonoBehaviour
+{
+    HingeJoint hinge;
+    JointSpring hingeSpring;
+    public string inputKeyName = "LeftPaddle";
+
+    // Use this for initialization
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        hingeSpring = hinge.spring;
+        hingeSpring.spring = 200000f;
+        hingeSpring.damper = 0;
+
+        if (Input.GetAxis(inputKeyName) == 1) {
+            hingeSpring.targetPosition = -45;
+        } else {
+            hingeSpring.targetPosition = 0;
+        }
+
+        hinge.spring = hingeSpring;
+        
+
+    }
+
+}
---
+++ Assets/PlungerScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlungerScript : MonoBehaviour
+{
+    float power = 0;
+    float minPower = 0;
+    float maxPower = 100;
+    Rigidbody ball;
+    public Slider powerSlider;
+
+    // Start is called before the first frame update
+    void Start() {
+        powerSlider.minValue = 0;
+        powerSlider.maxValue = maxPower;
+        // ball = new <Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Debug.Log("Log 1");
+        powerSlider.value = power;
+        if (Input.GetKeyDown("s")) {
+            
+                
+
+                Debug.Log("Log 4");
+                if (power <= maxPower)
+                {
+                    power += 50 * Time.deltaTime;
+                }
+            
+        }
+        if (Input.GetKeyUp("s")) {
+                
+
+            
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        
+        if (other.gameObject.CompareTag("TheBall")) {
+            
+            ball = other.gameObject.GetComponent<Rigidbody>();
+            ball.AddForce(1000 * Vector3.forward, ForceMode.Impulse);
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        
+    }
+}
